8dd3c75da5b3959badd470102b2c6abb08459afe
Sebastian009w <ia3266505@gmail.com>
2023-02-19 22:44:57 -0500

    resolviendo el famoso ejercicio fizz buzz

--- src/02-exercises.py
+++ src/02-exercises.py
@@ -1,1 +1,12 @@
 ## exercises of python #
+
+def fizz__buzz ():
+    for index in range(1, 101):
+      if index % 3 == 0 and index % 5 == 0:
+        print("fizzbuz: ", index)
+      elif index % 5 == 0:
+        print("buzz: ", index)
+      elif index % 3 == 0:
+        print("fizz: ", index)
+
+## realizado ##   
